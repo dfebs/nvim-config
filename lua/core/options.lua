9d3aa101c44fcec44584c1fefbba4ff8fe19d914
dfebs <daniel.febles97@gmail.com>
2025-07-22 19:40:23 -0400

    Put plugins and options in their own files

---
+++ lua/core/options.lua
@@ -0,0 +1,36 @@
+vim.opt.clipboard = "unnamedplus"
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.autoindent = true
+vim.o.breakindent = true -- Enable break indent
+vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.o.smartcase = true -- smart case
+vim.wo.signcolumn = 'yes' -- Keep signcolumn on by default
+vim.o.updatetime = 250 -- Decrease update time
+vim.o.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.o.backup = false -- creates a backup file
+vim.o.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.o.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+vim.opt.termguicolors = true -- set termguicolors to enable highlight groups
+vim.o.whichwrap = 'bs<>[]' -- which "horizontal" keys are allowed to travel to prev/next line
+vim.o.scrolloff = 4 -- minimal number of screen lines to keep above and below the cursor
+vim.o.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
+vim.o.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.o.tabstop = 4 -- insert n spaces for a tab
+vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations
+vim.o.expandtab = true -- convert tabs to spaces
+vim.o.splitbelow = true -- force all horizontal splits to go below current window
+vim.o.splitright = true -- force all vertical splits to go to the right of current window
+vim.o.swapfile = false -- disables swapfile creation
+vim.o.smartindent = true -- make indenting smarter again
+vim.o.showtabline = 2 -- always show tabs
+vim.o.backspace = 'indent,eol,start' -- allow backspace on
+vim.o.pumheight = 10 -- pop up menu height
+vim.o.fileencoding = 'utf-8' -- the encoding written to a file
+vim.opt.shortmess:append 'c' -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append '-' -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim in case vim still in use
+vim.o.cursorline = true -- highlight the current line
