e97ba56e3b99134305c68993393224628255f549
dfebs <daniel.febles97@gmail.com>
2025-07-25 10:33:10 -0400

    Add hide filter for neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,6 +12,12 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        never_show = { ".git" },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
