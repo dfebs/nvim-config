93e945f3fb43922791c60427a7d8aece1e5674b1
dfebs <daniel.febles97@gmail.com>
2025-08-07 17:24:36 -0400

    Add endwise plugin for ruby

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-require('core.options')
-require('core.keymaps')
+require("core.options")
+require("core.keymaps")
 
 -- Check if user is on mac instead of WSL. Is this highly specific? Yes. Does it work for me? Also yes.
 local home = os.getenv("HOME")
@@ -21,32 +21,32 @@ if home and not home:match("^/Users/") then
   }
 end
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
+    error("Error cloning lazy.nvim:\n" .. out)
   end
 end
 
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup({
-    require('plugins/neo-tree'),
-    require('plugins/color-theme'),
-    require('plugins/telescope'),
-    require('plugins/gitsigns'),
-    require('plugins/treesitter'),
-    require('plugins/alpha'),
-    require('plugins/indent-blankline'),
-    require('plugins/bufferline'),
-    require('plugins/lualine'),
-    require('plugins/lsp'),
-    require('plugins/lazydev'),
-    require('plugins/autocompletion'),
-    require('plugins/blame'),
-    require('plugins/autoformat'),
+require("lazy").setup({
+  require("plugins/neo-tree"),
+  require("plugins/color-theme"),
+  require("plugins/telescope"),
+  require("plugins/gitsigns"),
+  require("plugins/treesitter"),
+  require("plugins/alpha"),
+  require("plugins/indent-blankline"),
+  require("plugins/bufferline"),
+  require("plugins/lualine"),
+  require("plugins/lsp"),
+  require("plugins/lazydev"),
+  require("plugins/autocompletion"),
+  require("plugins/blame"),
+  require("plugins/autoformat"),
+  require("plugins.endwise"),
 })
-
