3a0889fc547ee88d609d9523a993ac5c60c50cea
dfebs <daniel.febles97@gmail.com>
2025-08-25 09:41:49 -0400

    Add css ftplugin

---
+++ ftplugin/css.lua
@@ -0,0 +1,3 @@
+vim.bo.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.bo.tabstop = 2 -- Number of spaces that a <Tab> counts for
+vim.bo.softtabstop = 2 -- Number of spaces that a <Tab> feels like
