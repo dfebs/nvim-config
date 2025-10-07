fe60f8cb385641b6bb2b31a9f072efacf5ee62db
dfebs <daniel.febles97@gmail.com>
2025-10-07 10:51:24 -0400

    Add cucumber to ftplugin

---
+++ ftplugin/cucumber.lua
@@ -0,0 +1,3 @@
+vim.bo.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.bo.tabstop = 4 -- Number of spaces that a <Tab> counts for
+vim.bo.softtabstop = 4 -- Number of spaces that a <Tab> feels like
