7e82321eafffb5fb2bdecc21983c41068b2331a5
dfebs <daniel.febles97@gmail.com>
2026-06-08 09:25:20 -0400

    Add ruff_format for python

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -31,9 +31,8 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { "stylua" },
       rust = { "rustfmt" },
+      python = { "ruff_format" },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
